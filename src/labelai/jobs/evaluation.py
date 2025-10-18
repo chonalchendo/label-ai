2cf925f29375315b4d0dfd493695038e46972d88
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-10-18 12:00:56 +0100

    ✨ feat: add EvaluationJob class for processing and analyzing training data with majority vote logic

---
+++ src/labelai/jobs/evaluation.py
@@ -0,0 +1,58 @@
+import typing as T
+from collections import Counter
+
+import numpy as np
+import polars as pl
+import pydantic as pdt
+
+import labelai.datasets as datasets
+import labelai.jobs.base as base
+
+
+class EvaluationJob(base.Job):
+    KIND: T.Literal["evaluation"] = "evaluation"
+
+    input_train_df: datasets.ReaderKind = pdt.Field(..., discriminator="KIND")
+
+    threshold: int
+
+    @T.override
+    def run(self) -> base.Locals:
+        # 1. load in labelled samples
+        train_df = self.input_train_df.read()
+
+        # 2. Count the number of records with a majority vote and those with not
+        labels = train_df.filter(pl.col("majority_vote") != "NO_CONCENSUS")[
+            "majority_vote"
+        ].to_list()
+
+        labels_no_concensus_count = train_df.filter(
+            pl.col("majority_vote") == "NO_CONCENSUS"
+        ).shape[0]
+
+        unlabelled_rows_pct = (
+            round(labels_no_concensus_count / train_df.shape[0], 2) * 100
+        )
+
+        # 4. Calculate the max, min, avg number of documents per class.
+        # 5. Calculate the number of classes with fewer than n documents
+        label_counts = Counter(labels)
+        counts = list(label_counts.values())
+        num_classes = len(label_counts)
+        avg_docs_per_class = max_docs = min_docs = classes_with_less_than_threshold = 0
+
+        if num_classes > 0:
+            avg_docs_per_class = np.mean(counts)
+            max_docs = np.max(counts)
+            min_docs = np.min(counts)
+            classes_with_less_than_threshold = sum(
+                1 for count in counts if count < self.threshold
+            )
+
+        print(f"Average number of documents per class: {avg_docs_per_class:.2f}")
+        print(f"Maximum number of documents in a class: {max_docs}")
+        print(f"Minimum number of documents in a class: {min_docs}")
+        print(
+            f"Number of classes with fewer than {self.threshold} documents: {classes_with_less_than_threshold}"
+        )
+        print(f"Percentage of records with NO_CONCENSUS: {unlabelled_rows_pct}")
